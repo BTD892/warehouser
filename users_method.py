e2adbd24d5699d4fe5d6669c34bf5cd49dcceba4
BTD892 <60697700+BTD892@users.noreply.github.com>
2021-11-17 17:22:21 +0800

    Update users_method.py

--- users_method.py
+++ users_method.py
@@ -1,18 +1,37 @@
+# 调用实例
+
+# 返回用户的浏览历史记录
+# UserMethod.view_history(userid)
+
+# 返回用户的收藏记录
+# UserMethod.view_mark()
+
+# 用户添加作品至收藏,并返回更新后的收藏记录
+# UerMethod.add_mark(userId, workId)
+
+# 用户删除收藏作品，并返回更新后的收藏记录
+# UserMethod.delete_mark(userId, workId)
+
+# 用户浏览作品后添加至历史记录
+# UserMethod.view_work(userId, workId)
+from user import User
+
+
 class UserMethod:
 
-    def register(self, UserId, phoneNumber, userName, profilePicture, userMarks='', userWorks='', userSearchRecord='', userPrefer=''):
+    def register(self, userId, phoneNumber, userName, profilePicture, userMarks='', userWorks='', userSearchRecord='', userPrefer=''):
         # 插入用户信息
-        # users = User.insertUserInfo(userId,phoneNumber,userName,profilePicture = '',userMarks = '',
-        # userWorks = '',userSearchRecord = '',userPrefer='')
-        print("This is Register!!!")
+        user = User.insertUserInfo(userId,phoneNumber,userName,profilePicture = '',userMarks = '',
+        userWorks = '',userSearchRecord = '',userPrefer='')
         pass
+        return user
         # 注册用户
 
     def view_work(self, userId, workId):
         # 添加用户历史浏览记录
-        # new_history = User.insertuserSearchRecord(userId,workId)
+        new_history = User.insertuserSearchRecord(userId,workId)
         # 仍需获取work信息
-        pass
+        return new_history
         # 浏览作品
 
     def publish_work(self):
@@ -21,35 +40,31 @@ class UserMethod:
 
     def view_history(self, userId):
         # 获取用户历史浏览记录
-        # now_history = User.getUserSearchRecord(userId)
-        # return now_history
-        pass
+        now_history = User.getUserSearchRecord(userId)
+        return now_history
         # 查看历史
 
     def view_mark(self, UserId):
         # 获取用户收藏信息
-        # now_marks = User.getuserMarks(userId)
-        # return now_marks
+        now_marks = User.getuserMarks(userId)
+        return now_marks
         pass
         # 查看收藏
 
     def delete_mark(self, userId, workId):
         # 删除用户收藏记录
-        # new_marksUser.deleteuserMarks(userId,workId)
-        # return new_marks
-        pass
+        new_marks = User.deleteuserMarks(userId,workId)
+        return new_marks
         # 删除收藏
 
     def delete_history(self, userId, workId):
         # 删除用户历史浏览记录
-        # new_history = User.deleteuserSearchRecord(userId, workId)
-        # return new_history
-        pass
+        new_history = User.deleteuserSearchRecord(userId, workId)
+        return new_history
         # 删除历史
 
     def add_mark(self, userId, workId):
         # 添加用户收藏记录
-        # new_mark = User.insertuserMarks(userId,workId)
-        # return new_mark
-        pass
+        new_mark = User.insertuserMarks(userId,workId)
+        return new_mark
         # 添加收藏
