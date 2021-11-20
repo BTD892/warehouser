43e21d5739fab5d57bdcf67f1e78a227af05f77a
BTD892 <60697700+BTD892@users.noreply.github.com>
2021-11-20 10:55:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,75 @@
 # coding=utf-8
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, redirect, request, jsonify, session, send_from_directory
 from Works.works_method import WorksMethod, AudioWorkMethod, ArticleWorkMethod
 from Users.users_method import UserMethod
 import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'TPmi4aLWRbyVq8zu9v82dWYW1'
 
 
-@app.route('/alwaysRight/reg', methods=["POST"])
-def reg():
+@app.route('/alwaysRight/reg', methods=["POST"])  # need put in
+def register():
     getData = request.get_json()
+    phone = getData.get('phone')
+    username = getData.get('username')
+    session['phone']=phone
+    result = UserMethod.register(phone, username)
+    print(result)
+    return jsonify(msg="user Phone Put in !")
+
+
+@app.route('/alwaysRight/setInfoByUserId', methods=["POST"])  # need put in
+def setInfoByUserId():
+    ingore = 1
+    getData = request.get_json()
+    phone = session['phone']
     nickName = getData.get("nickName")
     country = getData.get("country")
     province = getData.get("province")
     city = getData.get("city")
     gender = getData.get("gender")
     language = getData.get("language")
-    avatarUrl = getData.get("avatarUrl")
+    if nickName:
+        UserMethod.update_username(phone, nickName)
+        print("?")
+    if country:
+        ingore = 1
+    if province:
+        ingore = 1
+    if city:
+        ingore = 1
+    if gender:
+        ingore = 1
+    if language:
+        ingore = 1
 
-    userData = {
-        'nickName': nickName,
-        'country': country,
-        'province': province,
-        'city': city,
-        'gender': gender,
-        'language': language,
 
-    }
-    return jsonify(msg="ok")
+    return jsonify(msg="User Info put in !")
+
+
+@app.route('/alwaysRight/logIn', methods=["POST"])
+def login():
+    getData = request.get_json()
+    phone = getData.get("phone")
+    session['phone'] = phone
+    return jsonify(msg="session login ! ")
+
+
+@app.route('/alwaysRight/logOut', methods=["POST"])
+def logOut():
+    session.clear()
+    return jsonify(msg="session erase ! ")
+
+
+@app.route('/alwaysRight/checkLogin', methods=["POST"])
+def checkLogin():
+    message = 1
+    phone = session['phone']
+    if not phone:
+        message = 0
+
+    return jsonify(msg=message)
 
 
 @app.route('/alwaysRight/getRandomWork', methods=["POST"])
@@ -42,6 +84,13 @@ def getRandomWork():
 #     func()
 #
 #     return jsonify()
+@app.route('/alwaysRight/getUserHistory', methods=["POST"])
+def getUserHistory():
+    getData = request.get_json()
+    id = getData.get("id")
+    print(id)
+    userData = UserMethod.view_history(str(id))
+    return jsonify(randomWork=userData)
 
 
 @app.route('/alwaysRight/getUserLikeById', methods=["POST"])
@@ -50,8 +99,30 @@ def getUserLikeById():
     print(getData)
     id = getData.get("id")
     userData = UserMethod.view_mark(id)
-
     return jsonify(randomWork=userData)
+
+
+# @app.route('/alwaysRight/changeUserIcon')
+# def changeicon():
+#     getData = request.get_json()
+#     filePath = getData.get("filePath")
+@app.route('/alwaysRight/saveUserIcon')  # need put in
+def saveUserIcon():
+    Icon = request.files.get('Icon')
+    id = session.get('phone')
+    path = ".\\static\\Icon\\"
+    IconName = Icon.filename
+    filePath = path + IconName
+    Icon.save(filePath)
+    return jsonify(msg='save !')
+
+
+@app.route('/alwaysRight/getSomethingByUrl', methods=["POST"])  # need test
+def getSomethingByUrl():
+    getData = request.get_json()
+    url = getData.get("url")
+
+    return send_from_directory(url,  'black-diamond-suit_2666.png')
 
 
 @app.route('/alwaysRight/uploadAudio4Score', methods=["POST"])
@@ -60,14 +131,15 @@ def upload4Score():
     path = ".\\static\\audio\\"
     audioName = audio.filename
     filePath = path + audioName
-    userId = 2
+    userId = "2"
     print(type(userId))
     # 插入时必须有用户id
     audio.save(filePath)
     print(audioName)
+    fileType = "0"
     # thisAudioId = func()  # save id
     # thisAudioId2Score = func()  # save score
-    thisAudioId = WorksMethod.publish_public_work(audioName, filePath, userId)
+    thisAudioId = WorksMethod.publish_public_work(audioName, filePath, userId, fileType)
     thisAudioId2Score = WorksMethod.make_comment(str(thisAudioId))
     data = {
         'id': thisAudioId,
@@ -82,11 +154,12 @@ def upload4text():
     path = ".\\static\\audio\\"
     audioName = audio.filename
     filePath = path + audioName
-    userId = 2
+    userId = "2"
+    fileType = "0"
     # 插入时必须得有用户Id
     print(filePath)
     audio.save(filePath)
-    thisAudioId = WorksMethod.publish_public_work(audioName, filePath, userId)  # save id
+    thisAudioId = WorksMethod.publish_public_work(audioName, filePath, userId, fileType)  # save id
     thisAudioId2Text = AudioWorkMethod.translate_work(filePath)  # save score
     # WorkMethod.make_score(id)
     data = {
