6b797e6870739f9cc394ca90ffc25b768588f5bb
BTD892 <60697700+BTD892@users.noreply.github.com>
2021-11-25 02:25:25 +0800

    Update works_method.py

--- Works/works_method.py
+++ Works/works_method.py
@@ -1,6 +1,7 @@
 # coding = utf-8
 from Works.webport import translate
 import random
+from Users.user import User
 from Works.Work import Work
 from Works.WorkComment import WorkComment
 from Works.makescore import get_score
@@ -12,14 +13,36 @@ class WorksMethod:
         result = Work.deleteWork(workid)
         return result
 
-    def get_random_work():
+    def get_random_text():
         # 先获取一个作品id，再通过作品id获取作品信息
-        tail = len(Work.getWorkInfo())
+        tail = Work.getTextcnt()
         work_id = random.randint(0, tail-1)
-        result = Work.getSingleWorkInfo(str(10000+work_id))
+        work_name = Work.getWorkName(str(10000+work_id))
+        work_authorid = Work.getWorkAuthor(str(10000+work_id))
+        work_authorname = User.getUsername(str(work_authorid))
+        result = dict()
+        result["name"] = work_name
+        result["id"] = 10000+work_id
+        result["author"] = work_authorname
         return result
-
         # 获取推送文
+
+    def get_work_content(workid):
+        work = Work.getSingleWorkInfo(str(workid))
+        return work
+
+    def get_Audiowork(cnt):
+        result = list()
+        tail = Work.getAudiocnt()
+        while cnt>0:
+            work_id = random.randint(0, tail-1)
+            work = Work.getSingleWorkInfo(str(20000+work_id))[0]
+            author_id = work["userId"]
+            author_name = User.getUsername(str(author_id))
+            work["author"] = author_name
+            result.append(work)
+            cnt -= 1
+        return result
 
     def make_comment(text):
         # 获取随机数
